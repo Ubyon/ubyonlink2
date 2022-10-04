2bad8f2cf347dd7b7e7d587ae0547a7fdaa8daa6
Hexin Wang <hexin.wang@ubyon.com>
2022-10-04 11:29:23 -0700

    Added k8s and docker installer scripts.

---
+++ installer/docker/install_ubyonlink.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+set -e
+#set -x
+
+if [ $(id -u) == 0 ] ; then
+  echo
+  echo "Cannot run $0 in root. Run in sudo user!"
+  echo
+  exit -1
+fi
+
+# Output directory to mark the installation complete.
+OUTDIR="${1:-.}"
+
+# Management FQDN.
+CORE_MGMT_FQDN="${1:-manage.ubyon.com}"
+
+# TrustGate FQDN that ubyonlink connects to.
+ULINK_SERVER_FQDN="${2:-edge-device.ubyon.com}"
+
+INSTALL_FINISHED="$OUTDIR/.install_ubyonlink"
+
+if [ -f $INSTALL_FINISHED ] ; then
+  echo "Install has already finished."
+  exit
+fi
+
+install_basic_packages()
+{
+  echo "==> Install basic OS packages."
+  sudo apt-get install -y uuid-runtime > /dev/null
+}
+
+install_docker_container()
+{
+  echo "==> Install docker container."
+
+  local mars_cluster_id="$1"
+  local mars_ulink_endpoint="$2"
+  local user_name=$(id -u -n)
+  local group_name=$(id -g -n)
+
+  sudo tee /etc/systemd/system/ubyonlink.service > /dev/null <<EOF
+[Unit]
+Description=UbyonLink
+After=docker.service
+
+[Service]
+TimeoutStartSec=0
+User=$user_name
+Group=$group_name
+ExecStartPre=/usr/bin/docker pull quay.io/ubyon/mars-ulink:1.0.0
+ExecStart=/usr/bin/docker run --rm --network host --name ubyonlink \\
+    quay.io/ubyon/mars-ulink:1.0.0 /home/ubyon/bin/mars \\
+    --mars_cluster_id=$mars_cluster_id \\
+    --mars_ulink_endpoint=$mars_ulink_endpoint \\
+    --v=0
+ExecStop=/usr/bin/docker stop ubyonlink
+Restart=always
+RestartSec=20
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  # Start ubyonlink docker container.
+  sudo systemctl start --no-block ubyonlink
+  sudo systemctl enable ubyonlink
+}
+
+install_ubyonlink()
+{
+  install_basic_packages
+  
+  local cluster_id=$(uuidgen)
+  install_docker_container $cluster_id $ULINK_SERVER_FQDN
+
+  echo
+  echo "==> Installation completed successfully."
+  echo "Please register your ubyonlink via: "
+  echo "  https://$CORE_MGMT_FQDN/ucms/register/$cluster_id"
+}
+
+mkdir -p "$OUTDIR"
+
+install_ubyonlink
+
+touch $INSTALL_FINISHED
+
+echo
---
+++ installer/k8s/install_ubyonlink.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+set -e
+
+#set -x
+
+# Output directory for the k8s yaml files.
+OUTDIR="${1:-.}"
+
+# Management FQDN.
+CORE_MGMT_FQDN="${1:-manage.ubyon.com}"
+
+# TrustGate FQDN that ubyonlink connects to.
+ULINK_SERVER_FQDN="${2:-edge-device.ubyon.com}"
+
+INSTALL_FINISHED="$OUTDIR/.install_ubyonlink"
+
+if [ -f $INSTALL_FINISHED ] ; then
+  echo "Install has already finished."
+  exit
+fi
+
+install_basic_packages()
+{
+  echo "==> Install basic OS packages."
+  sudo apt-get install -y uuid-runtime > /dev/null
+}
+
+install_k8s_container()
+{
+  echo "==> Install k8s container."
+
+  local mars_cluster_id="$1"
+  local mars_ulink_endpoint="$2"
+
+  cat > $OUTDIR/ubyonlink.yaml <<EOF
+apiVersion: apps/v1
+kind: StatefulSet
+metadata:
+  name: ubyonlink
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: ubyonlink
+  serviceName: ubyonlink
+  template:
+    metadata:
+      labels:
+        app: ubyonlink
+        version: 1.0.0
+    spec:
+      hostNetwork: true
+      containers:
+      - name: ubyonlink
+        imagePullPolicy: Always
+        image: quay.io/ubyon/mars-ulink:1.0.0
+        command: ["/home/ubyon/bin/mars"]
+        args: ["--mars_cluster_id=$mars_cluster_id",
+               "--mars_ulink_endpoint=$mars_ulink_endpoint",
+               "--v=0"]
+        env:
+          - name: MY_POD_NAME
+            valueFrom:
+              fieldRef:
+                fieldPath: metadata.name
+          - name: MY_POD_NAMESPACE
+            valueFrom:
+              fieldRef:
+                fieldPath: metadata.namespace
+          - name: MY_POD_IP
+            valueFrom:
+              fieldRef:
+                fieldPath: status.podIP
+EOF
+
+  kubectl apply -f $OUTDIR/ubyonlink.yaml
+}
+
+install_ubyonlink()
+{
+  install_basic_packages
+  
+  local cluster_id=$(uuidgen)
+  install_k8s_container $cluster_id $ULINK_SERVER_FQDN
+
+  echo
+  echo "==> Installation completed successfully."
+  echo "Please register your ubyonlink via: "
+  echo "  https://$CORE_MGMT_FQDN/ucms/register/$cluster_id"
+}
+
+mkdir -p "$OUTDIR"
+
+install_ubyonlink
+
+touch $INSTALL_FINISHED
+
+echo
