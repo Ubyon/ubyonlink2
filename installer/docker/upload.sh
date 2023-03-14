f0a68fb49b2e90334cda9056b7f7eadc0af937ce
Hexin Wang <hexin.wang@ubyon.com>
2023-03-14 07:05:17 -0700

    Docker and k8s upload to include the whole ubyonac directory.

--- installer/docker/upload.sh
+++ installer/docker/upload.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-docker logs ubyonac > /tmp/mars-ulink.log 2>&1 && tar cfz /tmp/mars-ulink.tgz -C /tmp mars-ulink.log && curl -XPOST -H "authorization: Bearer `grep 'token: ' ./ubyonac/configs/ubyonlink.yaml | awk '{print $2}'`" --data-binary @"/tmp/mars-ulink.tgz" https://upload.ubyon.com/logs
+docker logs ubyonac > ./ubyonac/mars-ulink.log 2>&1 && tar cfz /tmp/mars-ulink.tgz -C ./ubyonac . && curl -XPOST -H "authorization: Bearer `grep 'token: ' ./ubyonac/configs/ubyonlink.yaml | awk '{print $2}'`" --data-binary @"/tmp/mars-ulink.tgz" https://upload.ubyon.com/logs
--- installer/k8s/upload.sh
+++ installer/k8s/upload.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-kubectl logs ubyonac-0 > /tmp/mars-ulink.log && tar cfz /tmp/mars-ulink.tgz -C /tmp mars-ulink.log && curl -XPOST -H "authorization: Bearer `grep 'token: ' ./ubyonac/configs/ubyonlink.yaml | awk '{print $2}'`" --data-binary @"/tmp/mars-ulink.tgz" https://upload.ubyon.com/logs
+kubectl logs ubyonac-0 > ./ubyonac/mars-ulink.log 2>&1 && tar cfz /tmp/mars-ulink.tgz -C ./ubyonac . && curl -XPOST -H "authorization: Bearer `grep 'token: ' ./ubyonac/configs/ubyonlink.yaml | awk '{print $2}'`" --data-binary @"/tmp/mars-ulink.tgz" https://upload.ubyon.com/logs
